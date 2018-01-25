132dc3a7d8dedf1999234fabe12b31fd7d35205d
Krzysztof Wszelaki <krzys_wszel@interia.eu>
2018-01-25 21:15:15 +0100

    tringleArea

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var a = 2;
+var h = 4;
+var tringleArea = a * h / 2;
+alert('pole trójkąta= ' + tringleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
